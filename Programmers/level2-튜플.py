d72f566a1e87c57696c7d117852942915befcda2
강현영 <dalkang000@gmail.com>
2020-08-25 00:55:07 +0900

    update

--- Programmers/level2-튜플.py
+++ Programmers/level2-튜플.py
@@ -1,16 +1,10 @@
 def solution(s):
     answer = []
-    # 양 옆의 {{ }} 대괄호 두 개는 빼고 난 후에 남은 '},{'를 기준으로 split
-    edit_s = s[2:len(s)-2].split('},{')
+    # 양 옆의 {{ }} 대괄호 두 개는 빼고 난 후에 남은 '},{'를 기준으로 split, 리스트 길이로 sorted
+    sorted_s = sorted(list(s[2:len(s)-2].split('},{')), key=len)
 
-    # 리스트 길이를 기준으로 sort
-    sorted_array = sorted(edit_s, key=len)
-
-    # ','를 기준으로 split 한 리스트를 돌며 answer에 안 담긴 숫자들만 담기
-    for j in sorted_array:
-        check = list(map(int, j.split(',')))
-        for k in check:
-            if k not in answer:
-                answer.append(k)
+    # ','를 기준으로 split 한 리스트 set에서 answer를 빼서 겹치지 않는 것 담기
+    for s in sorted_s:
+        answer += list( set(map(int, s.split(','))) - set(map(int, answer)) )
 
     return answer
